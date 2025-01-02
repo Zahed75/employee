5b16c7ad73c60d9bb835c895e4cb0f624572cbe4
“Zahed75” <zahedhasan.cs@gmail.com>
2025-01-03 04:36:02 +0600

    Layout Setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,27 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {LayoutComponent} from './components/layout/layout.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
 
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path:'layout',
+    component:LayoutComponent,
+    children:[
+      {
+        path:'dashboard',
+        component:DashboardComponent
+      }
+    ]
+  }
 ];
