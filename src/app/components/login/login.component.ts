c2e12200a836ffe9aef74321f4b9a68ffded4b5d
“Zahed75” <zahedhasan.cs@gmail.com>
2025-01-03 22:23:22 +0600

    bug identified

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
+
 export class LoginComponent {
+
+  loginObj: any = {
+    email: '',
+    password: ''
+  }
+
+  router = inject(Router);
+
+  onLogin() {
+    console.log('Login object:', this.loginObj); // Debugging the login object
+    if (this.loginObj.email === 'admin@gmail.com' && this.loginObj.password === '1234') {
+      this.router.navigateByUrl('layout');
+    } else {
+      alert('Wrong credentials');
+    }
+  }
 
 }
