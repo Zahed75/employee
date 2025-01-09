b20cd602a886e63060a7ab2a374ca0ec74c5720c
“Zahed75” <zahedhasan.cs@gmail.com>
2025-01-10 02:31:48 +0600

    getAllUsers api integrate done

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    RouterOutlet,
+
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  isSideMenuExpanded: boolean = false; // Initial state
+
+  toggleSideMenu(): void {
+    this.isSideMenuExpanded = !this.isSideMenuExpanded;
+  }
+
+  constructor(private httpClient: HttpClient) {
+
+    this.getUsers();
+
+  }
+
+  getUsers(){
+    this.httpClient.get("https://app.bestelectronics.com.bd/api/v1/user/allUsers").subscribe((res:any)=>{
+
+    })
+  }
 
 }
